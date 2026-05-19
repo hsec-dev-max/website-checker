84087df4dbfc759674ae219f06c6e6d07cd20316
DARK <dark@kali.DARK>
2026-05-19 03:03:18 +0100

    Remove test file

--- test.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-         x = int(input("what is x? "))
-         print(100 / x)
-         break
-    except ValueError:
-         print("Error: That's not a number")
-    except ZeroDivisionError:
-         print("Error: Cannot divide by zero")
